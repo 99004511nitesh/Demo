a7a36db36b201139b336627f023e6af2dc356a19
Bharathgopal <bharathgopal75@gmail.com>
2021-06-10 11:26:19 +0530

    First commit

---
+++ test.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int function()
+{
+    printf("Welcome\n");
+    return 0;
+}
+int main()
+{
+    function();
+    return 0;
+}
---
+++ test1.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int function()
+{
+    return 0;
+}
+int main()
+{
+    printf("Welcome\n");
+    return 0;
+}
