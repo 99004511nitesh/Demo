988127be16db3fb6fae156625fe46fcc04787bdb
Bharath G <bharathgopal75@gmail.com>
2021-06-14 10:24:10 +0530

    Update test.c
    Changes done as Dev-2

--- test.c
+++ test.c
@@ -2,13 +2,13 @@
 /*
 	This is a test.c file with main and one function
 */
-int test_function()
+int test_function2()
 {
-    printf("Welcome\n");
+    printf("Chnages as Dev-2 \n");
     return 0;
 }
-int test_main()
+int test_main2()
 {
-    test_function();
+    test_function2();
     return 0;
 }
